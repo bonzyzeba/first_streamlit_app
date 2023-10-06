cadc9968c10a8f595aa0a83249fabcba79352a84
bonzyzeba <145483222+bonzyzeba@users.noreply.github.com>
2023-10-06 19:22:27 +0530

    Update streamlit_app.py
    to check

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title('My Parents New Healthy Diner')
 
 Streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamit.text('Kale, Spinach & Rocket Smoothie')
 streamit.text('Hard-Bolied Free-Range Egg')
